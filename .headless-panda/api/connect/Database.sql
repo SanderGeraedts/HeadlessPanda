cd9617c0c20d3f3b7a106369183130ee94348a36
SanderGeraedts <info@codepanda.nl>
2016-02-27 16:23:53 +0100

    added database script

---
+++ .headless-panda/api/connect/Database.sql
@@ -0,0 +1,97 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE if exists HP_SITE;
+DROP TABLE if exists HP_ROLE;
+DROP TABLE if exists HP_USER;
+DROP TABLE if exists HP_ARTICLE;
+DROP TABLE if exists HP_EDIT;
+DROP TABLE if exists HP_ITEM;
+DROP TABLE if exists HP_CATEGORY;
+DROP TABLE if exists HP_ART_CAT;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE HP_SITE(
+	Id				INT(10)				PRIMARY KEY auto_increment,
+	Title			VARCHAR(255)		NOT NULL,
+	Description		LONGTEXT			NULL,
+	Author			VARCHAR(255)		NULL,
+	Icon			VARCHAR(255)		NULL
+);
+
+CREATE TABLE HP_ROLE(
+	Id				INT(10)				PRIMARY KEY auto_increment,
+	Site_Id			INT(10)				NOT NULL,
+	Name			VARCHAR(255)		NOT NULL,
+
+	CONSTRAINT		UN_Name1			UNIQUE(Site_Id, Name),
+	CONSTRAINT		FK_Site_Id1			FOREIGN KEY (Site_Id) REFERENCES HP_SITE(Id)
+);
+
+CREATE TABLE HP_USER(
+	Id				INT(10)				PRIMARY KEY auto_increment,
+	Role_Id			INT(10)				NOT NULL,
+	Site_Id 		INT(10)				NOT NULL,
+	Name 			VARCHAR(255)		NOT NULL,
+	Username		VARCHAR(255) 		NOT NULL,
+	Password		VARCHAR(255)		NOT NULL,
+	Avatar			VARCHAR(255) 		NULL,
+	Url				VARCHAR(255)		NULL,
+	Bio				LONGTEXT 			NULL,
+
+	CONSTRAINT 		CK_PasswordLength	CHECK (LENGTH(Password) > 7),
+	CONSTRAINT		CK_PasswordUpper	CHECK (UPPER(Password) != Password),
+	CONSTRAINT		CK_PasswordLower	CHECK (LOWER(Password) != Password),
+	CONSTRAINT 		UN_Username1		UNIQUE(Site_Id, Username),
+	CONSTRAINT 		FK_Role_Id1			FOREIGN KEY (Role_Id) REFERENCES HP_ROLE(Id),
+	CONSTRAINT 		FK_Site_Id2 		FOREIGN KEY (Site_Id) REFERENCES HP_SITE(Id)
+);
+
+CREATE TABLE HP_ARTICLE(
+	Id 				INT(10)				PRIMARY KEY auto_increment,
+	Author_Id		INT(10)				NOT NULL,
+	Site_Id 		INT(10)				NOT NULL,
+	Title			VARCHAR(255)		NOT NULL,
+	Publish_Date	DATETIME			DEFAULT CURRENT_TIMESTAMP,
+	Featured_Img	VARCHAR(255)		NULL,
+
+	CONSTRAINT 		FK_User_Id1			FOREIGN KEY (Author_Id) REFERENCES HP_USER(Id),
+	CONSTRAINT 		FK_Site_Id3			FOREIGN KEY (Site_Id) REFERENCES HP_SITE(Id)
+);
+
+CREATE TABLE HP_EDIT(
+	Id 				INT(10)				PRIMARY KEY auto_increment,
+	Article_Id		INT(10)				NOT NULL,
+	Editor_Id		INT(10)				NOT NULL,
+	Edit_Date		DATETIME 			DEFAULT CURRENT_TIMESTAMP,
+
+	CONSTRAINT 		FK_Article_Id1		FOREIGN KEY (Article_Id) REFERENCES HP_ARTICLE(Id),
+	CONSTRAINT 		FK_User_Id2 		FOREIGN KEY (Editor_Id) REFERENCES HP_USER(Id)
+);
+
+CREATE TABLE HP_ITEM(
+	Id 				INT(10)				PRIMARY KEY auto_increment,
+	Article_Id 		INT(10)				NOT NULL,
+	Item_Type		VARCHAR(255)		NOT NULL,
+	Item_Order		INT(10)				NOT NULL,
+	Item_Text		LONGTEXT 			NOT NULL,
+
+	CONSTRAINT 		UN_Order			UNIQUE(Article_Id, Item_Order),
+	CONSTRAINT 		FK_Article_Id2 		FOREIGN KEY (Article_Id) REFERENCES HP_ARTICLE(Id)
+);
+
+CREATE TABLE HP_CATEGORY(
+	Id 				INT(10)				PRIMARY KEY auto_increment,
+	Parent_Id		INT(10)				NULL,
+	Site_Id 		INT(10)				NOT NULL,
+	Name 			VARCHAR(255)		NOT NULL,
+
+	CONSTRAINT 		FK_CategoryId1		FOREIGN KEY (Parent_Id) REFERENCES HP_CATEGORY(Id),
+	CONSTRAINT 		FK_Site_Id4			FOREIGN KEY (Site_Id) REFERENCES HP_SITE(Id)
+);
+
+CREATE TABLE HP_ART_CAT(
+	Article_Id 		INT(10)				NOT NULL,
+	Category_Id		INT(10)				NOT NULL,
+
+	CONSTRAINT 		FK_Article_Id3 		FOREIGN KEY (Article_Id) REFERENCES HP_ARTICLE(Id),
+	CONSTRAINT 		FK_CategoryId2		FOREIGN KEY (Category_Id) REFERENCES HP_CATEGORY(Id)
+);
